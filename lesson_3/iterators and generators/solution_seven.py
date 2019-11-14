243a293eaad78ef201797f80f17da9ce3bdb10e6
Vladimir <fairflays@gmail.com>
2019-11-14 23:59:53 +0300

    Create files for solutions for lesson 3 and 4
    You don't need to create them by yourself anymore

---
+++ lesson_3/iterators and generators/solution_seven.py
@@ -0,0 +1,10 @@
+# 7. 	Не используя интерпретатор, определить, что выведут следующий выражения:
+# 	>>> '__iter__' in dir(list())
+# 	>>> '__iter__' in dir(tuple)
+# 	>>> '__iter__' in dir({})
+# 	>>> '__iter__' in dict
+# 	>>> '__iter__' in ''
+# 	>>> '__iter__' in (x for x in range(5))
+# 	>>> '__iter__' in dir(_)
+# 	>>> '__iter__' in range(5)
+# 	>>> '__iter__' in dir((x for x in range(3)))
