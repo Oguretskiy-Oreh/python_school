dc4eb679ce12df873b226a45b9cb161fcab802f1
Oguretskiy-Oreh <63635687+Oguretskiy-Oreh@users.noreply.github.com>
2020-04-14 11:28:08 +0300

    Update and rename lesson_1/dict/solution_one.py to  lesson_1/ dict/solution_one.py

--- lesson_1/ dict/solution_one.py
+++ lesson_1/ dict/solution_one.py
@@ -8,3 +8,12 @@
 # 93289 Lauren
 # где можно найти очки, которые заработал пользователь.
 # Нужно создать словарь из этих данных, найти победителя.
+
+dict_of_players = {'Джек': 69485, 'Михаил': 95715, 'Алекс': 95715, 'Морган': 83647, 'Джерри': 197128, 'Николь': 95715, 'Лорен': 93289}
+values = []
+for value in dict_of_players.values():
+	values.append(int(value))
+	values.sort()
+for winner, value in dict_of_players.items():
+	if value == values[-1]:
+		print(winner, value)
